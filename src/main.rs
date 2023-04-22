ac1eb07e0ac0ee8f653c6bb66bb6939193f65348
jiajunCheng <chengjiajun20@gmail.com>
2023-04-23 00:39:32 +0800

    TODO: make different logger for different modules

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ use axum::{
     routing::get,
     Router,
 };
-use log::info;
+use log::{info, warn, error};
 use log4rs;
 
 #[tokio::main]
 async fn main() {
-    log4rs::init_file("../log4rs.yaml", Default::default()).unwrap();
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     info!("aabb");
+    warn!("ccdd");
+    error!("eeff");
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
