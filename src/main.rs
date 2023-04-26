ba482a6d4463930db06aef11f41c36c737173e33
jiajunCheng <chengjiajun20@gmail.com>
2023-04-26 12:51:17 +0800

    ready to work on maven

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use axum::Router;
+use axum::extract::Path;
+use axum::{
+    routing::{get}
+};
 pub mod models;
 pub mod crud;
 pub mod config;
@@ -5,25 +10,28 @@ pub mod schema;
 pub mod storage;
 // cargo run --example minio
 
-use awsregion::Region;
-use s3::creds::Credentials;
-use s3::{Bucket, BucketConfiguration};
-
 #[tokio::main]
 async fn main() {
-    let bucket_name = "gentoo";
-    let region_name = "us-east-1".to_string();
-    let endpoint = "http://localhost:9000".to_string();
-    let region =  Region::Custom { region: region_name, endpoint };
-    let config = BucketConfiguration::default();
-    let assess_key = "admin";
-    let secret_key = "admin123";
-    let cred = Credentials::new(Some(assess_key), Some(secret_key), None, None, None).unwrap();
-    
-    // // Async variant with `tokio` or `async-std` features
-    let create_bucket_response = Bucket::create_with_path_style(bucket_name, region, cred, config).await;
-    match create_bucket_response {
-        Ok(_) => {}
-        Err(err) => {println!("{:?}", err)}
-    }
+    // build our application with a single route
+    let app = Router::new()
+    .route("/", get(root))
+    .route("/repo/:user/:repo", get(repo_info));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "hello world"
+}
+
+
+async fn repo_info(Path((user_name, repo_name)): Path<(String, String)>) -> String {
+    format!(
+        "Repo: user_name: {} repo_name: {}",
+        user_name, repo_name
+    )
 }
--- src/storage/s3_test.rs
+++ src/storage/s3_test.rs
@@ -1,10 +1,9 @@
 #[cfg(test)]
 mod tests3 {
-    use crate::{storage::s3::{S3Bucket}};
+    use crate::storage::s3::S3Bucket;
     use awsregion::Region;
-    use futures::Future;
     use s3::creds::Credentials;
-    use s3::{BucketConfiguration, Bucket};
+    use s3::Bucket;
 
     #[tokio::test]
     async fn test_make_bucket() {
@@ -12,9 +11,8 @@ mod tests3 {
         let sercet_key = String::from("8GF5bSzSMNL6jDiKxqIcFWpnaLlZ2Gxk");
         let end_point = String::from("http://localhost:9000");
         let bucket_name = String::from("demp5");
-        let region_name = "us-east-1".to_string();
         // let _ = create_bucket(bucket_name, end_point, access_key, sercet_key).await;
-        let resp = S3Bucket::new(bucket_name, access_key, sercet_key, end_point);    
+        let _ = S3Bucket::new(bucket_name, access_key, sercet_key, end_point);    
     }
 
     #[tokio::test]
